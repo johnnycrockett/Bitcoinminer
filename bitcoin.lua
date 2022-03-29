e90774b1f2d492b866b37c23541ac8036d99266e
LeafCwM <102511599+LeafCwM@users.noreply.github.com>
2022-03-28 18:23:29 -0600

    Update bitcoin.lua

--- bitcoin.lua
+++ bitcoin.lua
@@ -1,23 +1,25 @@
 local snow = false
 local algo = false
-local clock = false
+local autoclock = false
 local boost = false
 local crate = false
 local gem = false
 local mineboost = false
+local flag = false
 
 local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
 
 local w = library:CreateWindow("Bitcoin Miner") -- Creates the window
 
-local b = w:CreateFolder("Snowflake Farm")
 local c = w:CreateFolder("Farms") -- Creates the folder(U will put here your buttons,etc)
 local h = w:CreateFolder("Auto Gem") -- Creates the folder(U will put here your buttons,etc)
+local b = w:CreateFolder("WalkSpeed Changer")
 
-
+--[[
 b:Toggle("Auto Snowflake", function(bool)
 snow = bool
 end)
+--]]
 
 h:Toggle("Auto Gem", function(bool)
 gem = bool
@@ -28,7 +30,7 @@ c:Toggle("Auto Algorithm", function(bool)
 end)
 
 c:Toggle("Auto Overclock", function(bool)
-	clock = bool
+	autoclock = bool
 end)
 
 c:Toggle("Auto Boost Star", function(bool)
@@ -43,9 +45,7 @@ c:Toggle("Auto Mining Boost", function(bool)
     mineboost = bool
 end)
 
-
-
-b:Slider("Walkspeed",{
+b:Slider("Walkspeed",{  
 	min = 10; -- min value of the slider
 	max = 200; -- max value of the slider
 	precise = true; -- max 2 decimals
@@ -57,26 +57,10 @@ end)
 
 
 while true do
-	wait()
+	wait(0.1)
 	local suc, err = pcall(function()
-
-        if mineboost then
-            local mineboostpath = game:GetService("Players").LeafCwm.PlayerGui.Boosts.TextButton.Bs.ct
-            if mineboostpath.text == "00:00" then
-                wait(1)
-                game:GetService("ReplicatedStorage").Events.CrystalBuy:FireServer("10 min Mining Boost")
-                wait(1)
-                game:GetService("ReplicatedStorage").Events.UseBoost:FireServer("10 min Mining Boost")
-            end
-            if mineboostpath.text == "00:01" then
-                wait(1)
-                game:GetService("ReplicatedStorage").Events.CrystalBuy:FireServer("10 min Mining Boost")
-                wait(1)
-                game:GetService("ReplicatedStorage").Events.UseBoost:FireServer("10 min Mining Boost")
-            end
-        end
         
-		if snow then
+		--[[if snow then
 			wait()
 			
 			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Teleports["a4-SolarisShop"].CFrame
@@ -85,15 +69,26 @@ while true do
 			wait(0.2)
 			fireproximityprompt(game:GetService("Workspace").SnowFlakes.SnowFlake.Part.ProximityPrompt)
 		end
+		--]]
 		
 		if algo then
-			wait()
-			
+		    local algocheck = game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.AlgNotif
 			local dagger = string.match(game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.MiningAlgorithms.Dagger.Rate.Text, '%d%p%d+')
 			local scrypt = string.match(game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.MiningAlgorithms.Scrypt.Rate.Text, '%d%p%d+')
 			local sha = string.match(game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.MiningAlgorithms["SHA-512"].Rate.Text, '%d%p%d+')
 			local bloxchain = string.match(game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.MiningAlgorithms.xs.Rate.Text, '%d%p%d+')
-						
+		    game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Position = UDim2.new(.85, 0, .523, 0)
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Apps.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Codes.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.MinStats.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Warp.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.PlotMang.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.FastCard.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Apps.Seti.Icon.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Apps.Cus.Icon.Visible = false
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.MiningAlgorithms.Visible = true
+            
+            game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Visible = false
 			if dagger > scrypt and dagger > sha and dagger > bloxchain then
 				wait(1)
 				local args = {[1] = 2}
@@ -118,23 +113,34 @@ while true do
 				local args = {[1] = 4}
 				game:GetService("ReplicatedStorage").Events.AlgoChang:FireServer(unpack(args))
 			end
+			else
+			    game:GetService("Players").LocalPlayer.PlayerGui.Phone.Frame.Visible = true
 		end
 		
-		if clock then
+		if autoclock then
+		    game:GetService("Players").LocalPlayer.PlayerGui.Energy.Frame.Position = UDim2.new(.5, 0, .5, 0)
+		    wait(0.1)
+		    game:GetService("Players").LocalPlayer.PlayerGui.Energy.Frame.Visible = false
 			local clockpath = game:GetService("Players").LocalPlayer.PlayerGui.Energy.Frame.Overclock.Overclock.Lock.AviTitle
 			if clockpath.Visible == false then
 			wait()
 			game:GetService("ReplicatedStorage").Events.Overclk:InvokeServer()
 			end
+			else
+			    game:GetService("Players").LocalPlayer.PlayerGui.Energy.Frame.Visible = true
 		end
 		
 		if boost then
-			if game:GetService("Players").LocalPlayer.PlayerGui.Boosts.Bar.TextLabel.Tim.Text == "" then
+		    local boostpath = game:GetService("Players").LocalPlayer.PlayerGui.Boosts.Bar.TextLabel.Tim
+			if boostpath.Text == "" then
 			    game:GetService("ReplicatedStorage").Events.ClaimFreeBoostStar:FireServer()
 			end
 		end
 		
 		if crate then
+		    game:GetService("Players").LocalPlayer.PlayerGui.Crates.Frame.Position = UDim2.new(.5, 0, .5, 0)
+		    wait(0.1)
+		    game:GetService("Players").LocalPlayer.PlayerGui.Crates.Frame.Visible = false
 			local normalpath = game:GetService("Players").LocalPlayer.PlayerGui.Crates.Frame.ClmN
 			local smallpath = game:GetService("Players").LocalPlayer.PlayerGui.Crates.Frame.ClmS
 			if normalpath.Visible == true then
@@ -145,28 +151,61 @@ while true do
 				local args = {[1] = true}
 				game:GetService("ReplicatedStorage").Events.ClmFrCrt:FireServer(unpack(args))
 			end
+			else
+			   game:GetService("Players").LocalPlayer.PlayerGui.Crates.Frame.Visible = true 
 		end
 		
 		if gem then
-			wait()
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(38, 7, 90)
-			wait(0.35)
-            game:GetService("ReplicatedStorage").Events.PlaceCrystaliser:InvokeServer()
-            wait(0.35)
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GemsSpawned.GreenCrystal.Part.CFrame
-			wait(0.35)
-			fireproximityprompt(game:GetService("Workspace").GemsSpawned.GreenCrystal.Part.ProximityPrompt)
-			wait(0.35)
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GemsSpawned.BlueCrystal.Part.CFrame
-			wait(0.35)
-			fireproximityprompt(game:GetService("Workspace").GemsSpawned.BlueCrystal.Part.ProximityPrompt)
-			wait(0.35)
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GemsSpawned.RedCrystal.Part.CFrame
-			wait(0.35)
-			fireproximityprompt(game:GetService("Workspace").GemsSpawned.RedCrystal.Part.ProximityPrompt)
-			wait(0.35)
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(38, 7, 90)
+		    local boardcheck = game:GetService("Players").LocalPlayer.PlayerGui.CrystaliserCountdown.CrystaliserNotReady
+		    local greencheck = game:GetService("Workspace").GemsSpawned:FindFirstChild("GreenCrystal")
+		    local bluecheck = game:GetService("Workspace").GemsSpawned:FindFirstChild("BlueCrystal")
+		    local redcheck = game:GetService("Workspace").GemsSpawned:FindFirstChild("RedCrystal")
+			if boardcheck.Visible == false then
+			    wait(0.1)
+			    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(38, 7, 90)
+		        wait(1)
+			    game:GetService("ReplicatedStorage").Events.PlaceCrystaliser:InvokeServer()
+			end
+			while game:GetService("Workspace").GemsSpawned.DataCost > 4 do
+			    if greencheck then
+			            wait(0.1)
+		        	    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GemsSpawned.GreenCrystal.Part.CFrame
+		          	    wait(0.1)
+		        	    fireproximityprompt(game:GetService("Workspace").GemsSpawned.GreenCrystal.Part.ProximityPrompt)
+			    end
+	        	if bluecheck then
+		        	    wait(0.1)
+		        	    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GemsSpawned.BlueCrystal.Part.CFrame
+		        	    wait(0.1)
+		        	    fireproximityprompt(game:GetService("Workspace").GemsSpawned.BlueCrystal.Part.ProximityPrompt)
+	        	end
+	        	if redcheck then
+		        	    wait(0.1)
+		        	    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").GemsSpawned.RedCrystal.Part.CFrame
+		        	    wait(0.1)
+		        	    fireproximityprompt(game:GetService("Workspace").GemsSpawned.RedCrystal.Part.ProximityPrompt)
+	        	end
+			end
 		end
+		
+		  if mineboost then
+            local mineboostpath = game:GetService("Players").LocalPlayer.PlayerGui.Boosts.TextButton.Bs.ct
+            local crystalcheck = game:GetService("Players").LocalPlayer.PlayerGui.ActiveMechanic.CrystalShop.CurrencyCounter.Amount.text
+            if tonumber(crystalcheck)> 71 then
+                if mineboostpath.text == "00:00" then
+                    wait(1)
+                    game:GetService("ReplicatedStorage").Events.CrystalBuy:FireServer("10 min Mining Boost")
+                    wait(1)
+                    game:GetService("ReplicatedStorage").Events.UseBoost:FireServer("10 min Mining Boost")
+                end
+                if mineboostpath.text == "00:01" then
+                    wait(1)
+                     game:GetService("ReplicatedStorage").Events.CrystalBuy:FireServer("10 min Mining Boost")
+                    wait(1)
+                    game:GetService("ReplicatedStorage").Events.UseBoost:FireServer("10 min Mining Boost")
+                end
+            end
+        end
 	end)
 	if not suc then
 		warn(err)
